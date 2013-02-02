fbd51dd6c2aca8c856361fb7766c75043e38d212
SquareFeet <luke.t.moody@googlemail.com>
2013-02-02 21:11:05 +0000

    Added support for Mozilla's AudioContext; alert user if browser doesn't support AudioContext at all.

--- modules/audio.io.js
+++ modules/audio.io.js
@@ -13,6 +13,16 @@ audio.io = {
 	oscTypes: ['sine', 'square', 'sawtooth', 'triangle'],
 
 	initialize: function() {
+		var context = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || false;
+
+		if(!context) {
+			alert(
+				"Your browser doesn't seem to support the Web Audio API. " +
+				"Please ensure you're using the latest version of Chrome or Firefox."
+			);
+			return;
+		}
+
 		this.context = new webkitAudioContext();
 		this.masterOut = this.context.destination;
 	}
